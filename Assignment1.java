3cff31b0cb71b0662b8f65a7ecb8eed1091adfb2
kcollopy <kelly.collopy21@gmail.com>
2025-02-05 13:56:34 -0500

    Step 2 add Fahrenheit to Celsius conversion

--- Assignment1.java
+++ Assignment1.java
@@ -2,6 +2,24 @@
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+
+    // ***********************************************************************************
+    // Step 1: Write initials in block letters **Come Bake Too**
+    System.out.println("KC");
+
+    //************************************************************************************
+    // Step 2: Convert Fahrenheite to Celsius
+    final int base = 32;
+    final double conversion_factor = 5.0 / 9.0;
+
+    double CelsiusTemp;
+    int FahrenheiteTemp = 32;                                    //Value to Convert
+
+    CelsiusTemp = conversion_factor * (FahrenheiteTemp - base);
+
+    System.out.println("Celsius Temperature: " + CelsiusTemp);
+
+    // ************************************************************************************
+    
   }
 }
