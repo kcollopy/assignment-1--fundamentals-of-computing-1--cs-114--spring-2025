51f9f978710bf1cdebed5d2fd972d790c3e9b8f0
kcollopy <kelly.collopy21@gmail.com>
2025-02-13 19:15:25 -0500

    Step 1:  create initials in block letters

--- Assignment1.java
+++ Assignment1.java
@@ -6,28 +6,37 @@ public class Assignment1 {
   public static void main(String[] args) {
 
     //***********************************************************************************
-    // Step 1: Write initials in block letters **Come Bake Too**
-    System.out.println("KC");
+    // Step 1: Write initials in block letters **Come Back Too**
+    System.out.println("KK   KKK         CCC");
+    System.out.println("KK  KK        CCCCCCCC");
+    System.out.println("KK KK        CCCC");
+    System.out.println("KKKK        CCC");
+    System.out.println("KK KK        CCCC");
+    System.out.println("KK  KK        CCCCCCCC");
+    System.out.println("KK   KKK        CCC");
+
+
+
 
     //************************************************************************************
     //Step 2: Convert Fahrenheite to Celsius
-    final int base = 32;
-    final double conversion_factor = 5.0 / 9.0;
+    final int BASE = 32;
+    final double CONVERSTION_FACTOR = 5.0 / 9.0;
 
     double CelsiusTemp;
     int FahrenheiteTemp = 32;                                    //Value to Convert
 
-    CelsiusTemp = conversion_factor * (FahrenheiteTemp - base);
+    CelsiusTemp = CONVERSTION_FACTOR * (FahrenheiteTemp - BASE);
 
     //************************************************************************************
     //Step 3: Reverse a five character string without the first and last characters
-    String phrase = "zredx";
+    String phrase = "zderx";
     String mutation1, mutation2, mutation3, mutation4, mutation5;
     
     mutation1 = phrase.substring( 1, 4);     //Deleting the first and last character of the phrase
     mutation2 = mutation1.replace('r', 'd' ) ;   //Replacing 'r' with'd' to read 'ded'
     mutation3 = mutation2.substring(1, 3);   //Creating a substring to read 'ed'
-    mutation4 = mutation1.substring(0,1);    //Creating another substring to read 'r'
+    mutation4 = mutation1.substring(2,3);    //Creating another substring to read 'r'
     mutation5 = mutation4.concat(mutation3);                     //Concacting mutation 4 and 3 to read 'red'
 
     //**************************************************************************************
